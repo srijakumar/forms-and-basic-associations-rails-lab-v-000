d03f130652fda01116d2d03edb468a78945bd315
Srija S <srijakumar7@gmail.com>
2019-05-25 19:55:50 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -17,4 +17,9 @@ class Song < ActiveRecord::Base
     self.try(:artist).try(:name)
   end
 
+  def artist_name =(name)
+    artist = Artist.find_or_create_by(name: name)
+    self.artist = artist
+  end
+
 end
